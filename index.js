3a0493db16ea9316159cfa76b1ef6e74bcc75c96
rubaiiyat <abirr1516@gmail.com>
2025-10-03 16:41:02 +0600

    create and show user data

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, Db } = require("mongodb");
 require("dotenv").config();
 const express = require("express");
 const app = express();
@@ -7,7 +8,6 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.MONGODB_URL;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +23,51 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const database = client.db("lms-server");
+    const userCollection = database.collection("users");
+    const adminCollection = database.collection("admin");
+    const courseCollection = database.collection("course");
+
+    // create users api
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      try {
+        const result = await userCollection.insertOne(user);
+        res.status(200).json({
+          message: "Inserted Successfull",
+          title: user.name,
+        });
+      } catch (error) {
+        res.status(404).json({
+          message: "Not Inserted",
+        });
+      }
+    });
+
+    // Show Users
+    app.get("/users", async (req, res) => {
+      try {
+        const result = await userCollection.find().toArray();
+        res.status(200).json({ result });
+      } catch (error) {
+        res.status(404).json({
+          message: "No User Found",
+        });
+      }
+    });
+
+    /*  // Admin info and create admin collection
+    app.post("/admin",async(req,res)=>{
+      const adminUser=req.body
+      try {
+        const result=await adminCollection.insertOne(adminUser)
+        res.status(200).j
+      } catch (error) {
+        
+      }
+    }) */
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
